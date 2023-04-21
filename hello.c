b88155058c612746078462be6c5e8bf115619ef8
Mariam Ali <130349422+iiiamaira@users.noreply.github.com>
2023-04-21 06:49:52 +0000

    Fri, Apr 21, 2023, 6:49 AM +00:00

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <cs5o.c>
+#include <cs5o.h>
 
 int main(void)
 {
 
 string name = get_string("what is your name:");
-printf("hello %s\n" ,name);
+printf("hello %\s" ,name);
 
 }
 
